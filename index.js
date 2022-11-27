282586fa4718e3bff0e1361bb6dcbdac293370c8
Biziarkin Iaroslav S <comradeyar@gmail.com>
2022-11-27 19:19:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const run = async () => {
                 if (req.url == '/') {
                     // (возвращается текущее значение счётчика, инкремента не происходит);
                     collection.findOne({type: "counter"})
-                        .then((doc) => res.end(String(doc.value)));
+                        // .then((doc) => res.end(String(doc.value)));
+                        .then((doc) => res.end('asdasdasdasdasdasd'));
                 } else if (req.url == '/stat') {
                     // (возвращается текущее значение счётчика, и происходит инкремент);
                     collection.findOne({type: "counter"})
